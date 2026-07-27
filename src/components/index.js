ce4cd41b3798c4edcb6a4664d2b60a2c9242e2f5
Gia Long <lygialong201@gmail.com>
2026-07-27 22:47:04 +0700

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Main } from "./Main";
-export { default as Expertise } from "./Expertise";
-export { default as Timeline } from "./Timeline";
-export { default as Project } from "./Project";
-export { default as Contact } from "./Contact";
+export { default as Navigation } from "./Navigation.tsx";
+export { default as Footer } from "./Footer.tsx";
+export { default as Main } from "./Main.tsx";
+export { default as Expertise } from "./Expertise.tsx";
+export { default as Timeline } from "./Timeline.tsx";
+export { default as Project } from "./Project.tsx";
+export { default as Contact } from "./Contact.tsx";
